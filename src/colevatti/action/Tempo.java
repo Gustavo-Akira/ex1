4213b4bcaa68a92abff264ed86da93f287631cdb
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2021-02-12 16:15:00 -0300

    Usando Foreach

--- src/colevatti/action/Tempo.java
+++ src/colevatti/action/Tempo.java
@@ -3,8 +3,7 @@ package colevatti.action;
 public class Tempo {
 	public Double getTempo(int[] array) {
 		double tempoTotal = System.nanoTime();
-		for(int x=0;x<array.length;x++) {
-			int y = array[x];
+		for(int n : array) {
 		}
 		double tempoFinal = System.nanoTime();
 		tempoTotal = tempoFinal - tempoTotal;
